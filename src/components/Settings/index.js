e14c163a58b1bea934211d60d3aa145717addca6
bolzano <ivanov.ivan.v@gmail.com>
2019-12-07 14:42:01 +0200

    Add Shared styles

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
-import CoinGrid from './CoinGridList';
 import CoinGridList from './CoinGridList';
 
 export default function() {
