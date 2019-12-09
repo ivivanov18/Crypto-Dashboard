20072207926d26819c4b1147fe4aa380ee7606a4
bolzano <ivanov.ivan.v@gmail.com>
2019-12-09 21:56:16 +0200

    - add Highchartstheme - and adapt it to current app theme - update HighchartsConfig and removed not needed configs

--- src/components/Dashboard/PriceChart.js
+++ src/components/Dashboard/PriceChart.js
@@ -3,6 +3,9 @@ import ReactHighcharts from 'react-highcharts';
 import highchartsConfig from './HighchartsConfig';
 import { Tile } from '../Shared/Tile';
 import { AppContext } from '../AppProvider';
+import HighchartsTheme from './HighchartsTheme';
+
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 function PriceChart() {
 	return (
